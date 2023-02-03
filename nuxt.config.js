4886ecb2146fbd5fbcc856b47de308cce849093d
VentsislavB <94957058+VentsislavB@users.noreply.github.com>
2023-02-03 10:27:33 +0200

    changed server settings

--- nuxt.config.js
+++ nuxt.config.js
@@ -59,6 +59,11 @@ export default defineNuxtConfig({
 		baseURL: BASE_URL,
 	},
 
+	server: {
+		port: 8000, // default: 3000
+		host: '0.0.0.0', // default: localhost
+	},
+
 	auth: {
 		strategies: {
 			local: {
